7e078d31f58da74a7b8207464c27357f3160d7c1
apache0505 <jiayulutsk@ukr.net>
2022-11-04 19:08:08 +0200

    ds

--- 21_dom/task_03/index.js
+++ 21_dom/task_03/index.js
@@ -1,6 +1,7 @@
 export const getItemsList = () => {
   const elementsList = document.querySelectorAll('.technology');
   console.dir(elementsList);
+  return elementsList;
 };
 
 export const getItemsArray = () => {
