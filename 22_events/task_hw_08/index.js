d697852c50a0cbf58de1f5c6fef942c1cb645ab0
apache0505 <jiayulutsk@ukr.net>
2022-11-07 12:53:00 +0200

    ge

--- 22_events/task_hw_08/index.js
+++ 22_events/task_hw_08/index.js
@@ -1,4 +1,6 @@
 const check = document.querySelector('.task-status');
-handleClick = event => console.log(event.target.checked);
-
+function handleClick(event) {
+  console.log(event.target.checked);
+  console.log(event.target.value);
+}
 check.addEventListener('change', handleClick);
