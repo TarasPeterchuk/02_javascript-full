8acf9a3214be390caa37f3753b387dce08a28e85
apache0505 <jiayulutsk@ukr.net>
2022-11-07 12:46:11 +0200

    da

--- 22_events/task_hw_07/index.js
+++ 22_events/task_hw_07/index.js
@@ -1,5 +1,5 @@
 const inputText = document.querySelector('.text-input');
-function handleClick() {
+function handleClick(event) {
   console.log(event.target.value);
 }
 inputText.addEventListener('change', handleClick);
