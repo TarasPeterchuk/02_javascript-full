bf3e98a1cb77b70d27ac4a8ef2b145fab42905f6
apache0505 <jiayulutsk@ukr.net>
2022-11-07 21:05:39 +0200

    da

--- 23_events_forms/task_hw_01/index.js
+++ 23_events_forms/task_hw_01/index.js
@@ -58,7 +58,7 @@ const createListEl = () => {
   if (taskInput.value !== '') {
     let obj = { text: taskInput.value, done: false };
     tasks.push(obj);
-    // taskInput.value = '';
+    taskInput.value = '';
     renderTasks(tasks);
   }
 };
