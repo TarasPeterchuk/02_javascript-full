30ecc045b70878dd7193716a10f1d5ce7bdbfa05
apache0505 <jiayulutsk@ukr.net>
2022-11-07 12:21:20 +0200

    ds

--- 22_events/task_hw_03/index.js
+++ 22_events/task_hw_03/index.js
@@ -1,21 +1,6 @@
-export const getTitle = () => {
-  const searchEl = document.querySelector('.title');
-  return searchEl.textContent;
-};
-export const getDescription = () => {
-  const searchEl = document.querySelector('.about');
-  return searchEl.innerText;
-};
-export const getPlans = () => {
-  const searchEl = document.querySelector('.plans');
-  return searchEl.innerHTML;
-};
-export const getGoal = () => {
-  const searchEl = document.querySelector('.goal');
-  return searchEl.outerHTML;
-};
-
-console.log(getTitle());
-console.log(getDescription());
-console.log(getPlans());
-console.log(getGoal());
+const checkboxElem = document.querySelector('.task-status');
+function checkToConsole() {
+  console.log(checkboxElem.checked);
+  console.log(checkboxElem.value);
+}
+checkboxElem.addEventListener('change', checkToConsole);
