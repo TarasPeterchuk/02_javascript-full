dc81f3279fdd4cc1fd766f5db3a97bc669e11a58
apache0505 <jiayulutsk@ukr.net>
2022-11-15 20:12:14 +0200

    2121

--- 33_сonneсt_with_server_HTTP/task_hw_01/index.js
+++ 33_сonneсt_with_server_HTTP/task_hw_01/index.js
@@ -1,18 +1,32 @@
-/*
- * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
- */
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
-export const makePromise = arg => {
-  // put you code here
-  const promiseFromFunc = new Promise(resolve => {
-    resolve(arg);
-  });
-  return promiseFromFunc;
-};
+// const renderTaskList = taskListsData => {
+//   console.log(taskListsData);
+//   return taskListsData;
+// };
+// const fetchTaskList = () => {
+//   return fetch(baseUrl).then(response => response.json());
+// };
 
-/*
- * пример использования
- */
-makePromise(17).then(number => {
-  console.log(number); // 17
+export function getTasksList() {
+  return fetch(baseUrl).then(response => response.json());
+}
+export function getTaskById(taskId) {
+  return fetch(baseUrl.concat(`/${taskId}`)).then(response => response.json());
+}
+
+getTasksList().then(tasksList => {
+  console.log(tasksList);
+});
+// ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
+
+// export function getTaskById(taskId) {
+//   return getTasksList().then(userData => {
+//     const resultArr = userData;
+//     return resultArr.filter(el => el.id === taskId);
+//   });
+// }
+
+getTaskById('1').then(taskData => {
+  console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
 });
