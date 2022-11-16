fb7ade78cb1563b941e0c287148f128f4ab698b3
apache0505 <jiayulutsk@ukr.net>
2022-11-16 13:46:14 +0200

    eqw

--- 34_complex_practice_HTTP/task_hw_01/index.js
+++ 34_complex_practice_HTTP/task_hw_01/index.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://6374b55948dfab73a4e65042.mockapi.io/todo_list/users/';
+const baseUrl = 'https://6374b55948dfab73a4e65042.mockapi.io/todo_list/users';
 export function getUsersList() {
   return fetch(baseUrl).then(response => response.json());
 }
@@ -34,39 +34,36 @@ export function updateUser(userId, userData) {
 }
 
 // examples
-// getUsersList().then(users => {
-//   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+getUsersList().then(users => {
+  console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
+});
+
+getUserById('7').then(userData => {
+  console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
+});
+
+const newUserData = {
+  email: 'cool@email.com',
+  firstName: 'Iron',
+  lastName: 'Man',
+  age: 42,
+};
+
+// createUser(newUserData).then(() => {
+//   console.log('User created');
 // });
 
-// // getUserById('2').then(userData => {
-// //   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
-// // });
+const updatedUserData = {
+  email: 'new@email.com',
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 17,
+};
 
-// const newUserData = {
-//   email: 'cool@email.com',
-//   firstName: 'Iron',
-//   lastName: 'Man',
-//   age: 42,
-// };
-
-// // createUser(newUserData).then(() => {
-// //   console.log('User created');
-// // });
-
-// const updatedUserData = {
-//   email: 'new@email.com',
-//   firstName: 'John',
-//   lastName: 'Doe',
-//   age: 17,
-// };
-
-// // updateUser('1', updatedUserData).then(() => {
-// //   console.log('User updated');
-// // });
-
-// deleteUser('8').then(() => {
-//   console.log('User deleted');
-//   getUsersList().then(users => {
-//     console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-//   });
+// updateUser('1', updatedUserData).then(() => {
+//   console.log('User updated');
 // });
+
+deleteUser('8').then(() => {
+  console.log('User deleted');
+});
