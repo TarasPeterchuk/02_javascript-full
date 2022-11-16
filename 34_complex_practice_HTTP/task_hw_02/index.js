2cb49ce775c93fb0326243137d60cc7cc6b709e4
apache0505 <jiayulutsk@ukr.net>
2022-11-16 16:51:41 +0200

    ds

--- 34_complex_practice_HTTP/task_hw_02/index.js
+++ 34_complex_practice_HTTP/task_hw_02/index.js
@@ -15,7 +15,7 @@ function createUser(userData) {
 
 const onFormSubmit = event => {
   event.preventDefault();
-  createUser(Object.fromEntries(new FormData(formElem))).then(response => alert(response));
+  createUser(Object.fromEntries(new FormData(formElem))).then(response => alert(response.json()));
   // .then(data => alert(data));
   formElem.reset();
   buttonElem.setAttribute('disabled', true);
